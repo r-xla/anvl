14ac9333290e4f7b2b39e73f99dbf685c010306f
Louis Aslett <louis.aslett@durham.ac.uk>
2026-07-29 23:49:19 +0100

    Test validating nv_qnorm, including exhaustive f32 sweep given new implementation

---
+++ benchmarks/nv_qnorm/nv_qnorm_f32_grad.R
@@ -0,0 +1,27 @@
+source("f32-sweep.R")
+
+library("anvl")
+
+grad <- jit(gradient(\(p, log_p = FALSE) sum(nv_qnorm(p, log_p = log_p)), wrt = "p"), static = "log_p")
+
+f <- function(x) ifelse(x >= 0.0 & x <= 1.0, as.double(grad(nv_array(x, dtype="f32"))$p), 0.0)
+g <- function(x) ifelse(x >= 0.0 & x <= 1.0, 1.0/dnorm(qnorm(x)), 0.0)
+
+f32_sweep(f, g, "nv_qnorm_f32_grad.txt")
+
+library(reticulate)
+
+jax_qnorm_grad <- function(p) {
+  jax     <- import("jax", convert = FALSE)
+  jnp     <- import("jax.numpy", convert = FALSE)
+  jstats  <- import("jax.scipy.stats", convert = FALSE)
+  np      <- import("numpy")
+
+  x <- jnp$atleast_1d(jnp$asarray(p, dtype = jnp$float32))
+  g <- jax$vmap(jax$grad(jstats$norm$ppf))(x)
+
+  as.double(np$asarray(g))
+}
+f <- function(x) ifelse(x >= 0.0 & x <= 1.0, as.double(jax_qnorm_grad(x)), 0.0)
+
+f32_sweep(f, g, "nv_qnorm_f32_grad_jax.txt")
